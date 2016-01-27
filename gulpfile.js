a6a7694d75835f3a14bbfa0901d178811589ed20
Artem Denysov <denysov.artem@gmail.com>
2016-01-27 14:01:28 +0200

    add version 0.1.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var rigger = require('gulp-rigger');
+var clean = require('gulp-clean');
 
-gulp.task('default', function () {
+gulp.task('build', function () {
 	gulp.src('src/*.js')
+		.pipe(clean())
 		.pipe(rigger())
 		.pipe(gulp.dest('dist/'));
 });
